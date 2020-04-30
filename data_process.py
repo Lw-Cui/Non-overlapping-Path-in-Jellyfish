a903abd8afe3649b9f076b4ee32bbb21abe2641b
None <None>
2020-04-30 15:55:34 -0700

    add prompt

--- data_process.py
+++ data_process.py
@@ -8,8 +8,8 @@ def average(lst):
 def main():
     f = open(sys.argv[1], "r")
     lines = [l for l in f.readlines() if l.startswith("[SUM]")]
-    print(''.join(x for x in lines))
-    print (average([float(l.split()[5]) for l in lines]))
+    print(''.join(x for x in lines)) 
+    print("[REPORT] The average throughput per server is {} Mbits/sec".format(average([float(l.split()[5]) for l in lines])))
 
 
 if __name__ == "__main__":
